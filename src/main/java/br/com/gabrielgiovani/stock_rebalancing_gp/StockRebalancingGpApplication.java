ad9d60ca5e9e36d1d63d1a42ae76a2145a56202b
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-07-02 22:23:24 -0300

    Creating project

---
+++ src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/StockRebalancingGpApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielgiovani.stock_rebalancing_gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRebalancingGpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockRebalancingGpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrielgiovani/stock_rebalancing_gp/StockRebalancingGpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielgiovani.stock_rebalancing_gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRebalancingGpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
