349daf4cc3e3888d92854d994da8fb5082153f18
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-07-07 00:26:48 -0300

    Creating interface CRUDService and implementing in the class WalletService

---
+++ src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/services/CRUDService.java
@@ -0,0 +1,20 @@
+package br.com.gabrielgiovani.stock_rebalancing_gp.services;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface CRUDService<T> {
+
+    List<T> findAll();
+
+    List<T> findByFilters(Map<String, Object> filters);
+
+    Optional<T> findById(Integer id);
+
+    void saveOrUpdate(T entity);
+
+    void deleteById(Integer id);
+
+    void saveAll(List<T> entities);
+}
--- src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/services/WalletService.java
+++ src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/services/WalletService.java
@@ -6,19 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
-public class WalletService {
+public class WalletService implements CRUDService<Wallet> {
 
     @Autowired
     private WalletRepository walletRepository;
 
+    @Override
     public List<Wallet> findAll() {
         return walletRepository.findAll();
     }
 
-    public void saveAll(List<Wallet> wallets) {
-        walletRepository.saveAll(wallets);
+    @Override
+    public List<Wallet> findByFilters(Map<String, Object> filters) {
+        return null;
     }
 
+    @Override
+    public Optional<Wallet> findById(Integer id) {
+        return walletRepository.findById(id);
+    }
+
+    @Override
+    public void saveOrUpdate(Wallet entity) {
+        walletRepository.save(entity);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        walletRepository.deleteById(id);
+    }
+
+    @Override
+    public void saveAll(List<Wallet> entities) {
+        walletRepository.saveAll(entities);
+    }
 }
