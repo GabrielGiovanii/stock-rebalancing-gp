1fe39a2075b4f4497172843f3d76e2d9280eb025
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-07-11 20:06:09 -0300

    Adding initialization loading of categories in TestConfig

--- src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/config/TestConfig.java
+++ src/main/java/br/com/gabrielgiovani/stock_rebalancing_gp/config/TestConfig.java
@@ -1,6 +1,8 @@
 package br.com.gabrielgiovani.stock_rebalancing_gp.config;
 
+import br.com.gabrielgiovani.stock_rebalancing_gp.entities.Category;
 import br.com.gabrielgiovani.stock_rebalancing_gp.entities.Portfolio;
+import br.com.gabrielgiovani.stock_rebalancing_gp.services.CategoryService;
 import br.com.gabrielgiovani.stock_rebalancing_gp.services.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,18 +16,23 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
-    private PortfolioService walletService;
+    private PortfolioService portfolioService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @Override
     public void run(String... args) throws Exception {
         Portfolio p1 = new Portfolio("Renda Variável",
-                "Focada em ações com potencial de alta valorização e/ou pagamento de dividendos.",
-                100.0);
-
+                "Focada em ações com potencial de alta valorização e/ou pagamento de dividendos.", 100.0);
         Portfolio p2 = new Portfolio("Variando Positivo",
-                "Focada em ações que pagam bons dividendos.",
-                100.0);
+                "Focada em ações que pagam bons dividendos.", 100.0);
+        portfolioService.saveAll(Arrays.asList(p1, p2));
 
-        walletService.saveAll(Arrays.asList(p1, p2));
+        Category c1 = new Category("Total Return",
+                "Empresas sólidas no mercado que pagam dividendos.", 60.0);
+        Category c2 = new Category("Small Cap",
+                "Empresas pequenas com grande potencial de valorização.", 40.0);
+        categoryService.saveAll(Arrays.asList(c1, c2));
     }
 }
